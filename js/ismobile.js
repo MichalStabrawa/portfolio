6fa29ff45a5fafa4fe39ffcc4c8ce2972bf6dac7
Michal Stabrawa <mistaaqua@gmail.com>
2021-01-25 23:10:48 +0100

    refactor ismobile.js and change some css file

--- js/ismobile.js
+++ js/ismobile.js
@@ -2,25 +2,22 @@ const phoneLink = document.querySelector("a.phone");
 
 const isMobile = () => {
   if (
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
   ) {
     return null;
   } else {
-    
-    handleEvent(phoneLink,'click',phonePrevent);
+    handleEvent(phoneLink, "click", phonePrevent);
   }
 };
 
 const phonePrevent = (e) => {
   e.preventDefault();
-  alert('Dziala');
 };
 
-const handleEvent = (element,event,value) => {
+const handleEvent = (element, event, value) => {
   element.addEventListener(event, value);
 };
 
-export {
-  isMobile,handleEvent
-};
-
+export { isMobile, handleEvent };
